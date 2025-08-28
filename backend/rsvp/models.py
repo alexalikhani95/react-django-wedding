75c442c2c9f954b59cb9d0199441acdad3380a55
Alex Alikhani <alikhanialex@gmail.com>
2025-08-28 20:08:55 +0100

    Create migrations file with python manage.py makemigrations rsvp

--- backend/rsvp/models.py
+++ backend/rsvp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Rsvp(models.Model):
+    name = models.CharField(max_length=100)
+    attending = models.CharField(max_length=10)
+    starter = models.CharField(max_length=50, null=True, blank=True)
+    main = models.CharField(max_length=50, null=True, blank=True)
+    dessert = models.CharField(max_length=50, null=True, blank=True)
+    allergies = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.attending})"
+
