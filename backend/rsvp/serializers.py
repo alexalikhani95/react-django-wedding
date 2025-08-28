c830360f3039925f74ca3489fe2acbb868c2724e
Alex Alikhani <alikhanialex@gmail.com>
2025-08-28 20:17:18 +0100

    rsvp serializer

---
+++ backend/rsvp/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Rsvp
+
+class RsvpSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = Rsvp  
+      field = '__all__'
