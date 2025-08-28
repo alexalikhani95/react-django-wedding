1648310df080f96c08beb5545f56a3811a1c2a95
Alex Alikhani <alikhanialex@gmail.com>
2025-08-28 19:53:50 +0100

    create rsvp django app with command python manage.py startapp rsvp

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "corsheaders",
-    "rest_framework_simplejwt"
+    "rest_framework_simplejwt",
+    "rsvp"
 ]
 
 MIDDLEWARE = [
