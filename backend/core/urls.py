099d2428c99da1abc303602b82542c9bb1b52d47
Alex Alikhani <alikhanialex@gmail.com>
2025-09-14 16:29:10 +0100

    Add packages and update urls with /

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+
+def health(_request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rsvp.urls'))
+    path('api/', include('rsvp.urls')),
+    path("", health),  # <- optional: now "/" returns {"status":"ok"}
+
 ]
