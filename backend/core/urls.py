f214cbce694f76447cb6b9a69fad4f72b2a84bbc
Alex Alikhani <alikhanialex@gmail.com>
2025-09-14 17:05:33 +0100

    render.yaml file for djano backend render dploys

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -8,6 +8,6 @@ def health(_request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rsvp.urls')),
-    path("", health),  # <- optional: now "/" returns {"status":"ok"}
+    path("", health),  # To not make admin crash for now when visting just http://localhost:8000/ not http://localhost:8000/rsvp etc
 
 ]
