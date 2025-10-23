e89d9e5447dcb7dc16709f7e31328cae4bf9570c
Alex Alikhani <alikhanialex@gmail.com>
2025-10-23 19:29:57 +0100

    Testing setup files

--- frontend/src/routes/guests/guests.test.tsx
+++ frontend/src/routes/guests/guests.test.tsx
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom'
 import { expect, test } from 'vitest'
 import {screen} from '@testing-library/react'
 import { render } from '@/utils/test-utils'
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -11,4 +13,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.ts'
+  }
 });
