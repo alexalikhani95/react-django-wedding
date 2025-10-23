42b82dc81f7e047cbbc6aea4aeb6f3776294210f
Alex Alikhani <alikhanialex@gmail.com>
2025-10-23 19:09:06 +0100

    Basic guests test file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { ToastContainer } from "react-toastify";
 import App from "./App.tsx";
 import { Header } from "./components/Header.tsx";
-import { Guests } from "./routes/Guests.tsx";
+import { Guests } from "./routes/guests";
 import { Information } from "./routes/Information.tsx";
 import { RsvpList } from "./routes/RsvpList.tsx";
 import { Rsvp } from "./routes/rsvp";
