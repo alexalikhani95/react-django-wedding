7194919cd266440f42c981a55283e869ed231152
Alex Alikhani <alikhanialex@gmail.com>
2025-10-24 17:40:43 +0100

    Guests page integration tests

--- frontend/src/routes/guests/guests.test.tsx
+++ frontend/src/routes/guests/guests.test.tsx
@@ -1,11 +1,20 @@
 import '@testing-library/jest-dom'
 import { expect, test } from 'vitest'
-import {screen} from '@testing-library/react'
+import {screen, waitFor} from '@testing-library/react'
 import { render } from '@/utils/test-utils'
 import { Guests } from '.'
 
-test('Guests page', () => {
+test('Guests page renders', async () => {
     render(<Guests />)
 
     expect(screen.getByRole('heading', {name: 'Guests'})).toBeInTheDocument()
+
+    expect(screen.getByRole('button', {name: 'Add guest'})).toBeInTheDocument()
+
+    expect(screen.getByPlaceholderText('Search guest...')).toBeInTheDocument()
+
+    await waitFor(() => {
+    expect(screen.getByText('Alice')).toBeInTheDocument()
+    expect(screen.getByText('Bob')).toBeInTheDocument()
+  })
 })
