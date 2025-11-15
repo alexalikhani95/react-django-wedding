38ccc18a3425130ca948a279c14d1ed6bd8b1db2
Alex Alikhani <alikhanialex@gmail.com>
2025-11-15 10:34:02 +0000

    Update rsvp admin in accordance to model changes

--- backend/rsvp/admin.py
+++ backend/rsvp/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Rsvp
 
+
 @admin.register(Rsvp)
 class RsvpAdmin(admin.ModelAdmin):
-    list_display = ['name', 'attending', 'starter', 'main', 'dessert', 'created_at']
-    list_filter = ['attending', 'created_at']
-    search_fields = ['name']
-    readonly_fields = ['created_at']
+    list_display = [
+        "name",
+        "night_before",
+        "wedding_day",
+        "starter",
+        "main",
+        "dessert",
+        "allergies",
+        "created_at",
+    ]
+    list_filter = ["night_before", "wedding_day", "allergies", "created_at"]
+    search_fields = ["name"]
+    readonly_fields = ["created_at"]
