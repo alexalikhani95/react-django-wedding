79a105ceb500b86b168b9dd9fb1920655bc92a31
Alex Alikhani <alikhanialex@gmail.com>
2025-11-23 12:43:44 +0000

    Use react hook form in password screen

--- frontend/src/routes/rsvp/PinScreen.tsx
+++ frontend/src/routes/rsvp/PinScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { type SubmitHandler, useForm } from "react-hook-form"
 import LogoBeige from "@/assets/Logo-Biege.png"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -7,50 +7,63 @@ type Props = {
   setAccess: React.Dispatch<React.SetStateAction<string | null>>
 }
 
-export const PinScreen = ({ setAccess }: Props) => {
-  const [code, setCode] = useState("")
-  const [error, setError] = useState("")
+type FormInput = {
+  password: string
+}
 
-  const handleSubmit = () => {
+export const PinScreen = ({ setAccess }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormInput>()
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    const code = data.password.toLowerCase()
+
     if (code === "140826") {
       setAccess("weddingDay")
       localStorage.setItem("accessCode", "weddingDay")
-    }
-    if (code.toLowerCase() === "charlotte") {
+    } else if (code === "charlotte") {
       setAccess("charlotte")
       localStorage.setItem("accessCode", "charlotte")
-    } else if (code.toLowerCase() === "alikhaniwedding") {
+    } else if (code === "alikhaniwedding") {
       setAccess("nightBefore")
       localStorage.setItem("accessCode", "nightBefore")
     } else {
-      setError("Incorrect code. Please try again.")
+      setError("password", {
+        type: "manual",
+        message: "Incorrect password. Please try again.",
+      })
     }
   }
 
   return (
     <div className="bg-forest-green text-beige min-h-screen flex flex-col items-center gap-6 px-5 text-center justify-center">
       <img src={LogoBeige} className="w-[120px] opacity-80" />
-
       <h2 className="text-5xl font-mattedly">Welcome</h2>
       <p className="text-md font-evafiya max-w-[260px]">
         Please enter the password on your invitation to continue
       </p>
 
       <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center gap-6"
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center gap-4"
       >
         <Input
-          value={code}
-          onChange={(e) => {
-            setCode(e.target.value)
-            setError("")
-          }}
+          {...register("password", {
+            required: "Password is required",
+          })}
           placeholder="Enter password"
           className="bg-white max-w-[240px] text-forest-green font-evafiya"
         />
 
-        {error && <p className="text-red-600 text-sm font-adega">{error}</p>}
+        {errors.password && (
+          <p className="text-red-500 text-sm font-adega">
+            {errors.password.message}
+          </p>
+        )}
 
         <Button
           type="submit"
