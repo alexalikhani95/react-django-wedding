1918e4db46e82e98f62067814b75735783189e82
Alex Alikhani <alikhanialex@gmail.com>
2025-12-01 21:04:17 +0000

    Revert "Update menu section images"
    This reverts commit bfb772f776122113d870e6a9e4a5e0aae41b67fa.

--- frontend/src/routes/rsvp/components/MenuAndAllergies.tsx
+++ frontend/src/routes/rsvp/components/MenuAndAllergies.tsx
@@ -20,10 +20,11 @@ export const MenuAndAllergies = ({
   return (
     <div>
       <div
-        className={`grid grid-cols-1 sm:grid-cols-2 py-15 px-10 bg-cover bg-center bg-no-repeat ${nightBeforeAccess
-            ? "bg-[url(@/assets/Menu-bg-beige.jpg)]"
-            : "bg-[url(@/assets/Menu-bg-green.jpg)]"
-          }`}
+        className={`grid grid-cols-1 sm:grid-cols-2 py-15 px-10 bg-cover bg-center bg-no-repeat ${
+          nightBeforeAccess
+            ? "bg-[url(@/assets/Menu-beige.jpg)]"
+            : "bg-[url(@/assets/Menu-Green.jpg)]"
+        }`}
       >
         {/* Menu Card */}
         <div className="flex justify-center">
