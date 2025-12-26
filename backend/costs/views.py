002fa824c8f889193312b33917b14abeaa6a8b1b
Alex Alikhani <alikhanialex@gmail.com>
2025-12-26 14:36:48 +0000

    Cost views updated

--- backend/costs/views.py
+++ backend/costs/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import CostItem 
+from .serializers import CostSerializer
 
-# Create your views here.
+
+class CostItemCreate(generics.CreateAPIView):
+    queryset = CostItem.objects.all()
+    serializer_class = CostSerializer
+
+
+class CostList(generics.ListAPIView):
+    queryset = CostItem.objects.all()
+    serializer_class = CostSerializer
+
+
+class CostUpdate(generics.UpdateAPIView):
+    queryset = CostItem.objects.all()
+    serializer_class = CostSerializer
+
+
+class CostDelete(generics.DestroyAPIView):
+    queryset = CostItem.objects.all()
+    serializer_class = CostSerializer
